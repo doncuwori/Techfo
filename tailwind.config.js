9391fe6f14f0810fae91a929e32f7ca1c5311c33
elmiraa89 <liaelmira89@gmail.com>
2024-11-01 15:37:19 +0700

    fix config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,17 @@
-import defaultTheme from "tailwindcss/defaultTheme";
-import forms from "@tailwindcss/forms";
-
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 export default {
-    darkMode: "class",
-    content: [
-        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-        "./storage/framework/views/*.php",
-        "./resources/views//*.blade.php",
-        "./resources/js//*.jsx",
-    ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
-                inter: ["Inter", "sans-serif"],
-            },
-        },
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.jsx",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
     },
-
-    plugins: [forms],
-};
+  },
+  plugins: [],
+}
